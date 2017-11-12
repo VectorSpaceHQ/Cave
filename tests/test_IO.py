e52b331ee0b8b97729a73581ac51c0af2c6d3ac8
Adam Spontarelli <adam@vector-space.org>
2017-11-12 13:48:51 -0500

    Added init file to server package

--- tests/test_IO.py
+++ tests/test_IO.py
@@ -10,6 +10,8 @@ from unittest.mock import patch
 from unittest.mock import MagicMock
 import thermostat.RPiGetTemp as RPiGetTemp
 import thermostat
+import server
+from server.server import autoSetDaemon
 from thermostat.thermostat import thermDaemon
 import os
 
@@ -20,12 +22,20 @@ class BasicFunction(unittest.TestCase):
     def test_hot(self):
         # @patch(RPiGetTemp.getTemp, simple_getTemp)
         RPiGetTemp.getTemp = MagicMock(return_value=3)
-        print(simple_getTemp(70))
+
         abspath = os.path.abspath(__file__)
         dname = os.path.dirname(abspath)
-        daemon = thermDaemon(dname+'/thermDaemon.pid')
-        daemon.stop()
-        daemon.run()
+
+        
+        # server_daemon = autoSetDaemon(dname+'/autoSetDaemon.pid')
+        thermostat_daemon = thermDaemon(dname+'/thermDaemon.pid')
+        # server_daemon.stop()
+        # server_daemon.run()
+        thermostat_daemon.stop()
+        thermostat_daemon.run()
+
+
+        
         for i in range(10):
             print("adam")
             time.sleep(1)
