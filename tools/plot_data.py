5ed5ebb679b5653787edcf3e6685c57e88fc8c97
Adam Spontarelli <adam@vector-space.org>
2018-08-19 16:31:33 -0400

    major cleanup of plots

--- tools/plot_data.py
+++ tools/plot_data.py
@@ -29,9 +29,9 @@ def on_connect(client, userdata, flags, rc):
 def get_data():
     conn = mdb.connect(CONN_PARAMS[0],CONN_PARAMS[1],CONN_PARAMS[2],CONN_PARAMS[3],port=CONN_PARAMS[4])
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM ThermostatLog ORDER BY timeStamp DESC LIMIT 100")
+    cursor.execute("SELECT * FROM ThermostatLog ORDER BY timeStamp DESC LIMIT 1440")
     data = cursor.fetchall()
-    cursor.execute("SELECT * FROM SystemLog ORDER BY timeStamp DESC LIMIT 100")
+    cursor.execute("SELECT * FROM SystemLog ORDER BY timeStamp DESC LIMIT 1440")
     system_data = cursor.fetchall()
     Toutside = [x[1] for x in system_data]
     lowTarget = [x[2] for x in system_data]
@@ -53,26 +53,26 @@ def main():
     aux = [x[7] for x in data]
 
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2, figsize=(16,10), dpi=80)
-    ax1.plot(time, T_actual, 'o-', label='inside')
+    ax1.plot(time, T_actual, 'o-', markevery=54, label='inside')
     ax1.fill_between(time, lowTarget, highTarget, 
                      where=highTarget >= lowTarget, color='grey', alpha=0.5, label='comfort zone')
-    # ax1.plot(time, lowTarget, 'x-', label='low target')
-    # ax1.plot(time, highTarget, 'x-', label='high target')
-    ax1.plot(time, Toutside, 's-', label='outside')
+    ax1.plot(time, Toutside, 's-', markevery=63, color='purple', label='outside')
     
 
     # rotate and align the tick labels so they look better
     fig.autofmt_xdate()
     
-    ax1.legend(loc='best')
+    ax1.legend(loc="lower left")
     ax1.set_ylabel("Temperature (F)")
     ax1.set_xlabel("Time")
     
     ax1.xaxis.set_major_formatter(mdates.DateFormatter("%m-%d-%y %H:%m"))
-
-    ax2.plot(time, cool, '-s', markevery=7, label='cool')
-    ax2.plot(time, heat, '-o', markevery=5, label='heat')
-    ax2.plot(time, fan, '-x', markevery=3, label='fan')
+    bool_cool = ['On' if x==1 else 'Off' for x in cool]
+    bool_heat = ['On' if x==1 else 'Off' for x in heat]
+    bool_fan = ['On' if x==1 else 'Off' for x in fan]
+    ax2.plot(time, bool_cool, '-s', markevery=57, label='cool')
+    ax2.plot(time, bool_heat, '-o', markevery=65, label='heat')
+    ax2.plot(time, bool_fan, '-x', markevery=73, label='fan')
     ax2.plot(time, aux)
 
     
@@ -80,9 +80,9 @@ def main():
     integrated_cool = integrate.cumtrapz(cool, inc_times, initial=0)
     integrated_fan = integrate.cumtrapz(fan, inc_times, initial=0)
     integrated_heat = integrate.cumtrapz(heat, inc_times, initial=0)
-    ax3.plot(list(reversed(time)), integrated_cool/60, '-s', markevery=7, label='cool')
-    ax3.plot(list(reversed(time)), integrated_fan/60, '-x', markevery=3, label='fan')
-    ax3.plot(list(reversed(time)), integrated_heat/60, '-o', markevery=5, label='heat')
+    ax3.plot(list(reversed(time)), integrated_cool/60, '-s', markevery=57, label='cool')
+    ax3.plot(list(reversed(time)), integrated_heat/60, '-o', markevery=75, label='heat')
+    ax3.plot(list(reversed(time)), integrated_fan/60, '-x', markevery=63, label='fan')
     ax3.legend(loc='best')
     ax3.set_xlabel("Time")
     ax3.set_ylabel("On Time (min)")
@@ -90,11 +90,11 @@ def main():
 
 
     ax4.plot(list(reversed(time)), 100*(integrated_cool/(np.array(inc_times)- inc_times[0])),
-             '-s', markevery=7, label='cool')
-    ax4.plot(list(reversed(time)), 100*(integrated_fan/(np.array(inc_times)- inc_times[0])),
-             '-x', markevery=3, label='fan')
+             '-s', markevery=57, label='cool')
     ax4.plot(list(reversed(time)), 100*(integrated_heat/(np.array(inc_times)- inc_times[0])),
-             '-o', markevery=5, label='heat')
+             '-o', markevery=75, label='heat')
+    ax4.plot(list(reversed(time)), 100*(integrated_fan/(np.array(inc_times)- inc_times[0])),
+             '-x', markevery=63, label='fan')
     ax4.set_xlabel("Time")
     ax4.set_ylabel("Percent")
     ax4.legend(loc='best')
@@ -114,8 +114,9 @@ def main():
     ax2.grid(True)
     ax3.grid(True)
     ax4.grid(True)
-    plt.show()
-    plt.savefig("Thermostat_Report.png")
+    plt.suptitle("Thermostat Data, 24-hr period", fontsize=16)
+    # plt.show()
+    fig.savefig("Thermostat_Report.png", dpi=150)
     
 
 if __name__ == '__main__':
