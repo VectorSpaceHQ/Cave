264e74a2cebcb40b5c8d7d2d1f88ab8af2af92f0
Adam Spontarelli <adam@vector-space.org>
2018-09-09 12:17:21 -0400

    begin updating database

--- thermostat/database.py
+++ thermostat/database.py
@@ -18,22 +18,9 @@ CONN_PARAMS = (config.get('main','mysqlHost'), config.get('main','mysqlUser'),
         int(config.get('main','mysqlPort')))
 
 
-class Database(FysomGlobalMixin):
-    GSM = FysomGlobal(
-        events=[('cool', 'idle', 'cool'),
-                ('heat',  'idle', 'heat'),
-                ('fan', 'idle', 'fan'),
-                ('idle', ['cool', 'heat'], 'idle')],
-        initial='idle',
-        state_field='state'
-    )
-    # GSM = FysomGlobal({ 'initial': 'idle',
-    #               'events': [
-    #               {'name': 'cool', 'src': 'idle', 'dst': 'cool'},
-    #               {'name': 'heat', 'src': 'idle', 'dst': 'heat'},
-    #               {'name': 'stop', 'src': ['cool', 'heat'], 'dst': 'idle'},
-    #               {'name': 'circulate', 'src': 'idle', 'dst': 'fan'} ] })
-    
+class Database():
+
+
     def __init__(self):
         self.last_update = 0
         self.connected = False
@@ -69,6 +56,29 @@ class Database(FysomGlobalMixin):
         cursor.close()
         conDB.commit()
         conDB.close()
+
+        
+    def get_sensor_data(self):
+        """
+        Return sensor data from the SensorData table.
+        """
+
+        conn = mdb.connect(CONN_PARAMS[0],CONN_PARAMS[1],CONN_PARAMS[2],CONN_PARAMS[3],port=CONN_PARAMS[4])
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM SensorData ORDER BY readingID DESC LIMIT 50")
+        sensor_data = cursor.fetchall()
+        cursor.close()
+
+        if len(sensor_data) == 0:
+            print("ERROR: There is no sensor data.")
+            print("       Get your thermostat up and running first.")
+            sys.exit()
+
+        last_sensor_time = sensor_data[-1][1]
+        self.T_in = float(sensor_data[0][-4])
+
+        return sensor_data
+
 
 if __name__ == "__main__":
     db = Database()
